358fa929c060fe3b5c203192b8b4f2be2a51aa61
Aadarsh Gupta <Aadarsh.Gupta@Prolifics.com>
2024-05-21 15:22:25 +0530

    Max_subarray_sum (Brute_force)

---
+++ Array/Kadane's Algorithm/Max_subarray_sum.java
@@ -0,0 +1,24 @@
+class Max_subarray_sum{
+
+    // Brute force approach O(n^3)
+    public static int maxSubaraySum(int[] arr,int len){
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    sum+=arr[k];
+                }
+                maxSum=Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        
+        int[] arr={-2,1,-3,4,-1,2,1,-5,4};
+        int len=arr.length;
+        int maxsum=maxSubaraySum(arr,len);
+        System.out.println("The maximum subarray sum is :" +maxsum);
+    }
+}
