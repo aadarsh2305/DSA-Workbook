89fe0ab43da5090ebebcf2a86d907aec6652c301
Aadarsh Gupta <Aadarsh.Gupta@Prolifics.com>
2024-05-21 15:36:51 +0530

    Max_subarray_sum (2 pointer approach )

--- Array/Kadane's Algorithm/Max_subarray_sum.java
+++ Array/Kadane's Algorithm/Max_subarray_sum.java
@@ -1,6 +1,6 @@
 class Max_subarray_sum{
 
-    // Brute force approach O(n^3)
+    /* // Brute force approach O(n^3)
     public static int maxSubaraySum(int[] arr,int len){
         int maxSum = Integer.MIN_VALUE;
         for(int i=0;i<len;i++){
@@ -13,7 +13,22 @@ class Max_subarray_sum{
             }
         }
         return maxSum;
-    }
+    } */
+
+    
+    // Better approach 2 pointer approach O(n^2)
+    public static int maxSubaraySum(int[] arr,int len){
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<len;i++){
+            int sum=0;
+            for(int j=i;j<len;j++){
+                sum+=arr[j];
+                maxSum=Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    } 
+    
     public static void main(String[] args) {
         
         int[] arr={-2,1,-3,4,-1,2,1,-5,4};
